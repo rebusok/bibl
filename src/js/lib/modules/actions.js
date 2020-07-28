d13e5f1fe9ba963bb7e086b37c8d220519f29d65
Yuriy <rebusok@yandex.ru>
2020-07-28 16:38:51 +0300

    two

---
+++ src/js/lib/modules/actions.js
@@ -0,0 +1,69 @@
+import $ from '../core';
+
+$.prototype.on =  function (eventName, callBack){
+    for (let i = 0; i < this.length; i++){
+        if (!this[i].addEventListener){
+            continue;
+        }
+        if(!eventName || !callBack){
+            return this;
+        }
+        this[i].addEventListener(eventName, callBack);
+    }
+    return this;
+};
+
+$.prototype.off = function (eventName, callBack){
+    for (let i = 0; i < this.length; i++){
+        if (!this[i].addEventListener){
+            continue;
+        }
+        if(!eventName || !callBack){
+            return this;
+        }
+        this[i].removeEventListener(eventName, callBack);
+    }
+    return this;
+};
+
+$.prototype.click = function (handler){
+    for (let i = 0; i < this.length; i++){
+        if (!this[i].addEventListener){
+            continue;
+        }
+        if (handler){
+            this[i].addEventListener('click', handler);
+        } else{
+            this[i].click();
+        }
+        
+    }    
+    return this;    
+};
+
+// атрибуты 
+$.prototype.addAtribute = function (name, value) {
+    for (let i = 0; i < this.length; i++) {
+        this[i].setAttribute(name, value);
+    }
+    return this;
+};
+ 
+$.prototype.removeAtribute = function (nameAtribute) {
+    for (let i = 0; i < this.length; i++) {
+        this[i].removeAttribute(nameAtribute);
+    }
+ 
+    return this;
+};
+
+$.prototype.toggleAtribute = function (nameAtribute, value){
+    for (let i = 0; i < this.length; i++) {
+        if (this[i].hasAttribute(nameAtribute)){
+            this[i].removeAttribute(nameAtribute);
+        } else {
+            this[i].setAttribute(nameAtribute, value);
+        }
+    }
+    return this;
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-console.log('Hello!');
+import './lib/lib';
+import $ from './lib/lib';
+
+
+$('button').on('click', function(){
+    $(this).toggleClass('active');
+});
+$('div').addAtribute('woooj', 300).toggleAtribute('woooj').toggleAtribute('woooj', 600);
+
