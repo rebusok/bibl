b709cbebfbac84483c9c7b1901c0f71f3d00ca8c
Yuriy <rebusok@yandex.ru>
2020-08-03 17:33:30 +0300

    finish lib

--- src/js/main.js
+++ src/js/main.js
@@ -2,48 +2,49 @@ import './lib/lib';
 import $ from './lib/lib';
 
 
-$('#first').on('click', () =>{
-    $('div').eq(1).fadeOut(800);
-});
-$('[data-coutn="second"]').on('click', () =>{
-    $('div').eq(2).fadeOut(800);
-});
+// $('#first').on('click', () =>{
+//     $('div').eq(1).fadeOut(800);
+// });
+// $('[data-coutn="second"]').on('click', () =>{
+//     $('div').eq(2).fadeOut(800);
+// });
 
-$('button').eq(2).on('click', () => {
-    $('.w-500').fadeOut(800);
-});
-$('button').eq(3).on('click', () => {
-    $('.w-500').fadeToggle(800);
-});
+// $('button').eq(2).on('click', () => {
+//     $('.w-500').fadeOut(800);
+// });
+// $('button').eq(3).on('click', () => {
+//     $('.w-500').fadeToggle(800);
+// });
 
-$('#trigger').click(() => $('#trigger').createModal(
-    {
-        textTitle: 'Modal title',
-        textBody: 'orem ipsum dolor sit amet consectetur adipisicing elit.',
-        btnCount: 3,
-        btnSetting:[
-            [
-                'Close',
-                ['btn-dangers', 'mr-10'],
-                true
-            ],
-            [
-                'Save changes',
-                ['btn-success'],
-                false,
-                ()=>{
-                    alert("save");
-                }
-            ],
-            [
-                'Extra btn',
-                ['btn-dark', 'ml-10'],
-                false,
-                () => {
-                    alert("warning");
-                }
-            ]
+// $('#trigger').click(() => $('#trigger').createModal(
+//     {
+//         textTitle: 'Modal title',
+//         textBody: 'orem ipsum dolor sit amet consectetur adipisicing elit.',
+//         btnCount: 3,
+//         btnSetting:[
+//             [
+//                 'Close',
+//                 ['btn-dangers', 'mr-10'],
+//                 true
+//             ],
+//             [
+//                 'Save changes',
+//                 ['btn-success'],
+//                 false,
+//                 ()=>{
+//                     alert("save");
+//                 }
+//             ],
+//             [
+//                 'Extra btn',
+//                 ['btn-dark', 'ml-10'],
+//                 false,
+//                 () => {
+//                     alert("warning");
+//                 }
+//             ]
             
-        ]
-    }
-));
+//         ]
+//     }
+// ));
+
